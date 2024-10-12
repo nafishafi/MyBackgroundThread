56df72107f7ad1090ea24dd556aa0a7b343ef6bc
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-10-12 14:50:40 +0700

    add thread test

--- app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dicoding.mybackgroundthread
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+        btnStart.setOnClickListener {
+            try {
+                //simulate process compressing
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+                    if (percentage == 100) {
+                        tvStatus.setText(R.string.task_completed)
+                    } else {
+                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                    }
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
         }
     }
 }
